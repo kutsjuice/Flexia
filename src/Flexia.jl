90a1354363c124904b477ba7431de0a5d44a8343
Kuts Michael <kutsjuice@gmail.com>
2025-06-02 23:33:51 +0300

    fix hinge joint - now compiles and run

--- src/Flexia.jl
+++ src/Flexia.jl
@@ -1,4 +1,5 @@
 # module Flexia
+using Pkg; Pkg.activate(".")
 using StaticArrays
 # using GLMakie
 # export MBSystem
@@ -31,18 +32,22 @@ const g = 9.81
 
 bd1 = Body2D(10, 1)
 bd2 = Body2D(10, 1)
+# bd2 = Body2D(10, 1)
 
 bd1.forces[2] = (x) -> -bd1.mass * g
 bd2.forces[2] = (x) -> -bd2.mass * g
 
 
 jnt1 = FixedJoint(bd1)
+jnt2 = HingeJoint(bd1, bd2)
+set_position_on_second_body!(jnt2, SA[-0.5, 0])
 
 sys = MBSystem2D()
 
 add!(sys, bd1)
 add!(sys, bd2)
 add!(sys, jnt1)
+add!(sys, jnt2)
 
 if (!assemble!(sys))
     println("Assembling failed!")
--- src/joints.jl
+++ src/joints.jl
@@ -21,12 +21,14 @@ function setrotation!(joint::FixedJoint, θ)
     return nothing
 end
 
-function add!(sys::MBSystem2D, joint::FixedJoint)
+number_of_dofs(::FixedJoint) = 3
+
+function add!(sys::MBSystem2D, joint::AbstractJoint2D)
     push!(sys.joints, joint)
     # sys.jointsnum += 1;
-    last_joint_dof = last_lm_dof(sys) + 3
+    last_joint_dof = last_lm_dof(sys) + number_of_dofs(joint)
     push!(sys.lmdofs, last_joint_dof)
-    joint.index = length(sys.joints)
+    setid!(joint, length(sys.joints))
 end
 
 function get_lms(sys::MBSystem2D, joint::FixedJoint)
@@ -54,13 +56,7 @@ function add_joint_to_rhs!(rhs, state, sys::MBSystem2D, joint::FixedJoint)
         last_body_dof,
     ]
 
-    last_joint_dof = get_lms(sys, joint)
-
-    joint_dofs = [
-        last_joint_dof - 2,
-        last_joint_dof - 1,
-        last_joint_dof,
-    ]
+    joint_dofs = get_lms(sys, joint)
 
     rhs[velocity_dofs[1]] += state[joint_dofs[1]]
     rhs[velocity_dofs[2]] += state[joint_dofs[2]]
@@ -77,12 +73,38 @@ mutable struct HingeJoint <: AbstractJoint2D
     body1_hinge_point::SVector{2,Float64}
     body2::Body2D
     body2_hinge_point::SVector{2,Float64}
+    index::Int64
 
+    function HingeJoint(bd1::Body2D, bd2::Body2D)
+        return new(bd1, SA[0.0, 0.0], bd2, SA[0.0, 0.0])
+    end
+end
+
+number_of_dofs(::HingeJoint) = 2
+function setid!(jnt::T, index) where T<:AbstractJoint2D
+    jnt.index = index
+end
+function set_position_on_first_body!(joint::HingeJoint, pos::SVector{2,Float64})
+    joint.body1_hinge_point = pos;
+    return nothing
+end
+
+function set_position_on_second_body!(joint::HingeJoint, pos::SVector{2,Float64})
+    joint.body2_hinge_point = pos;
+    return nothing
+end
+
+function get_lms(sys::MBSystem2D, joint::HingeJoint)
+    last_lm = sys.lmdofs[joint.index]
+    return SA[
+        last_lm-1,
+        last_lm-0,
+    ]
 end
 
 function add_joint_to_rhs!(rhs, state, sys::MBSystem2D, joint::HingeJoint)
-    bd1 = joints.body1
-    bd2 = joints.body2
+    bd1 = joint.body1
+    bd2 = joint.body2
 
     bd1_p_dofs = get_body_position_dofs(sys, bd1)
     bd1_v_dofs = get_body_velocity_dofs(sys, bd1)
@@ -124,9 +146,9 @@ function add_joint_to_rhs!(rhs, state, sys::MBSystem2D, joint::HingeJoint)
     rhs[bd2_v_dofs[3]] += (xcj * sin(_θj) + ycj * cos(_θj)) * λ1 +
                           (-xcj * cos(_θj) + ycj * sin(_θj)) * λ2
 
-    rhs[lm[1]] = (_xi + xci * cos(_θi) - yci * sin(_θi)) -
+    rhs[lms[1]] = (_xi + xci * cos(_θi) - yci * sin(_θi)) -
                  (_xj + xcj * cos(_θj) - ycj * sin(_θj))
-    rhs[lm[2]] = (_yi + xci * sin(_θi) + yci * cos(_θi)) -
+    rhs[lms[2]] = (_yi + xci * sin(_θi) + yci * cos(_θi)) -
                  (_yj + xcj * sin(_θj) + ycj * sin(_θj))
 
 
