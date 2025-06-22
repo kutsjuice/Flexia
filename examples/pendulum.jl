70177bb6c579927eb8ff146861920d766d3b156f
Kuts Michael <kutsjuice@gmail.com>
2025-06-22 17:41:54 +0300

    ref(examples): useFlexia function for animation

--- examples/pendulum.jl
+++ examples/pendulum.jl
@@ -1,4 +1,8 @@
+using Pkg; Pkg.activate("./examples")
 using Flexia
+using GLMakie
+using ForwardDiff
+
 using StaticArrays
 
 const g = 9.81
@@ -6,40 +10,50 @@ const g = 9.81
 bd1 = Body2D(10, 1)
 bd2 = Body2D(10, 1)
 bd3 = Body2D(10, 1)
+bd4 = Body2D(10, 1)
+bd5 = Body2D(10, 1)
 # bd2 = Body2D(10, 1)
 
 bd1.forces[2] = (x) -> -bd1.mass * g
 bd2.forces[2] = (x) -> -bd2.mass * g
-bd3.forces[2] = (x) -> -bd2.mass * g
+bd3.forces[2] = (x) -> -bd3.mass * g
+bd4.forces[2] = (x) -> -bd4.mass * g
+bd5.forces[2] = (x) -> -bd5.mass * g
 
 
 jnt1 = FixedJoint(bd1)
 jnt2 = HingeJoint(bd1, bd2)
 jnt3 = HingeJoint(bd2, bd3)
+jnt4 = HingeJoint(bd3, bd4)
+jnt5 = HingeJoint(bd4, bd5)
 set_position_on_second_body!(jnt2, SA[-0.5, 0])
 
 set_position_on_first_body!(jnt3, SA[0.5, 0])
 set_position_on_second_body!(jnt3, SA[-0.5, 0])
+set_position_on_first_body!(jnt4, SA[0.5, 0])
+set_position_on_second_body!(jnt4, SA[-0.5, 0])
+set_position_on_first_body!(jnt5, SA[0.5, 0])
+set_position_on_second_body!(jnt5, SA[-0.5, 0])
 
 sys = MBSystem2D()
 
 add!(sys, bd1)
 add!(sys, bd2)
 add!(sys, bd3)
+add!(sys, bd4)
+add!(sys, bd5)
 
 add!(sys, jnt1)
 add!(sys, jnt2)
 add!(sys, jnt3)
+add!(sys, jnt4)
+add!(sys, jnt5)
 
 if (!assemble!(sys))
     println("Assembling failed!")
 end
 # end # module Flexia
 func = sys.rhs
-
-
-
-using ForwardDiff
 
 jacoby = (x) -> ForwardDiff.jacobian(func, x)
 
@@ -60,34 +74,5 @@ end
 time_span = 0:0.01:10
 sol = Matrix{Float64}(undef, number_of_dofs(sys), length(time_span))
 cros!(sol, initial, mass, func, jacoby, step(time_span))
-#
-using GLMakie
 
-function GLMakie.lift(system, solution, body::Body2D, i::Observable)
-    return lift(i) do value
-        points = Vector{Point2f}(undef, 2)
-        points[1:2] .= get_boundary_points(system, body, solution[:, value])
-        return points
-    end
-end
-
-function animate(sys::MBSystem2D, sol, filename; framerate=60)
-    fig = Figure()
-    i = Observable(1)
-    ax = Axis(fig[1, 1])
-    bar1 = lift(sys, sol, bd1, i)
-    bar2 = lift(sys, sol, bd2, i)
-    bar3 = lift(sys, sol, bd3, i)
-
-    lines!(ax, bar1)
-    lines!(ax, bar2)
-    lines!(ax, bar3)
-    limits!(ax, -4, 4, -4, 4)
-
-    record(fig, filename, 1:5:length(time_span);
-        framerate=framerate) do t
-        i[] = t
-    end
-end
-
-animate(sys, sol, "time_animation2.mp4")
+animate(sys, sol, time_span, "time_animation2.mp4"; framerate = 30, limits = (-5,5, -5, 5))
