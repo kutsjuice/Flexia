3348ccbd16b6c73cf8d45132ecac5f43bc83b9c4
kkaduskin1579 <kkaduskin1579@gmail.com>
2025-11-19 23:36:34 +0300

    RM

--- src/visualize.jl
+++ src/visualize.jl
@@ -36,7 +36,8 @@ end
 
 function draw!(ax, joint::TorsionalSpring, system::MBSystem2D, solution, iter::Observable)
     hinge_point = lift(system, solution, joint, iter);
-    lines!(ax, hinge_point, color=:blue);
+    lines!(ax, hinge_point, color=:blue, linewidth=2);
+    scatter!(ax, hinge_point, color=:red, markersize=8);
 end
 
 function animate(sys::MBSystem2D, sol, time_span, filename; framerate=60, limits = (-1, 1, 1, 1))
