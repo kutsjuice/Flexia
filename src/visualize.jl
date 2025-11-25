d514b9b1d057de21e445ae1be4bfa9f98222724d
kkaduskin1579 <kkaduskin1579@gmail.com>
2025-11-25 18:54:55 +0300

    RM

--- src/visualize.jl
+++ src/visualize.jl
@@ -40,8 +40,10 @@ function Makie.lift(system, solution, joint::TorsionalSpring, i::Observable)
         start_angel = _θi1 + π
         end_angel = _θi2 + 4*π
 
-        r0 = 0.4
-        r1 = 0.6
+        n1 = 2
+
+        r0 = 0.4 / n1
+        r1 = 0.6 / n1
         N = 100
 
         t = LinRange(start_angel, end_angel, N)
