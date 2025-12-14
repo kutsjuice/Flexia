50861c42d0ed47a6fc9d1a97f878730dab44ceaa
kkaduskin1579 <kkaduskin1579@gmail.com>
2025-12-15 00:44:45 +0300

    fix(debug): prevent falling bodies

--- examples/real_5bar.jl
+++ examples/real_5bar.jl
@@ -7,9 +7,9 @@ using StaticArrays
 
 const g = 9.81
 
-bd01 = Body2D(10, 500,length = 2.)
-bd02 = Body2D(10, 500,length = 2.)
-bd03 = Body2D(10, 500,length = 2.)
+bd01 = Body2D(10, 500,length = 0.50)
+bd02 = Body2D(10, 500,length = 0.50)
+bd03 = Body2D(10, 500,length = 0.50)
 
 bd1 = Body2D(10, 500, length = 1.56)
 bd2 = Body2D(10, 500, length = 1.56)
@@ -17,18 +17,16 @@ bd3 = Body2D(10, 500, length = 0.50)
 bd4 = Body2D(10, 500, length = 1.56)
 bd5 = Body2D(10, 500, length = 1.56)
 
-# bd1.forces[2] = (x) -> -bd1.mass * g
-# bd2.forces[2] = (x) -> -bd2.mass * g
-# bd3.forces[2] = (x) -> -bd3.mass * g
-# bd4.forces[2] = (x) -> -bd4.mass * g
-# bd5.forces[2] = (x) -> -bd5.mass * g
-# bd01.forces[2] = (x) -> -bd6.mass * g
-# bd02.forces[2] = (x) -> -bd6.mass * g
-# bd03.forces[2] = (x) -> -bd6.mass * g
+bd1.forces[2] = (x) -> -bd1.mass * g
+bd2.forces[2] = (x) -> -bd2.mass * g
+bd3.forces[2] = (x) -> -bd3.mass * g
+bd4.forces[2] = (x) -> -bd4.mass * g
+bd5.forces[2] = (x) -> -bd5.mass * g
+bd01.forces[2] = (x) -> -bd01.mass * g
+bd02.forces[2] = (x) -> -bd02.mass * g
+bd03.forces[2] = (x) -> -bd03.mass * g
 
 jnt01 = FixedJoint(bd01)
-jnt02 = FixedJoint(bd02)
-jnt03 = FixedJoint(bd03)
 
 jnt11 = HingeJoint(bd01, bd1)
 jnt2 = HingeJoint(bd1, bd2)
@@ -38,14 +36,18 @@ jnt4 = HingeJoint(bd3, bd4)
 jnt5 = HingeJoint(bd4, bd5)
 jnt21 = HingeJoint(bd5, bd02)
 
-# tcp11 = TorsionalSpring(bd01, bd1, 1100.,0.0, 100.)
-# tcp2 = TorsionalSpring(bd1, bd2, 1100.,0.0, 100.)
-# tcp3 = TorsionalSpring(bd2, bd3, 1100.,0.0, 100.)
-# tcp31 = TorsionalSpring(bd03, bd3, 1100.,0.0, 100.)
-# tcp4 = TorsionalSpring(bd3, bd4, 1100.,0.0, 100.)
-# tcp5 = TorsionalSpring(bd4, bd5, 1100.,0.0, 100.)
-# tcp21 = TorsionalSpring(bd02, bd5, 1100.,0.0, 100.)
+jnt02 = FixedJoint(bd02)
+jnt03 = FixedJoint(bd03)
 
+tcp11 = TorsionalSpring(bd01, bd1, 1100.,0.0, 100.)
+tcp2 = TorsionalSpring(bd1, bd2, 1100.,0.0, 100.)
+tcp3 = TorsionalSpring(bd2, bd3, 1100.,0.0, 100.)
+tcp31 = TorsionalSpring(bd03, bd3, 1100.,0.0, 100.)
+tcp4 = TorsionalSpring(bd3, bd4, 1100.,0.0, 100.)
+tcp5 = TorsionalSpring(bd4, bd5, 1100.,0.0, 100.)
+tcp21 = TorsionalSpring(bd02, bd5, 1100.,0.0, 100.)
+
+set_position_on_first_body!(jnt11, SA[0.5, 0])
 set_position_on_second_body!(jnt11, SA[-1.56, 0.])
 
 set_position_on_first_body!(jnt2, SA[1.56, 0])
@@ -61,10 +63,14 @@ set_position_on_first_body!(jnt5, SA[1.56, 0])
 set_position_on_second_body!(jnt5, SA[-1.56, 0])
 
 set_position_on_first_body!(jnt21, SA[1.56, 0])
-set_position_on_first_body!(jnt31, SA[0., 0])
+set_position_on_second_body!(jnt21, SA[0.5, 0])
 
-jnt02.pos = SA[2.7062, 0.0]
-jnt03.pos = SA[1.3531, 0.0]
+set_position_on_first_body!(jnt31, SA[0., 0])
+set_position_on_second_body!(jnt31, SA[0.5, 0])
+
+jnt01.pos = SA[-0.5, 0.0]
+jnt02.pos = SA[1.3531+1+1+0.5+1, 0.0]
+jnt03.pos = SA[1.3531+1, 1.56+1.56+0.78+1.]
 
 sys = MBSystem2D()
 
@@ -77,6 +83,10 @@ add!(sys, bd01)
 add!(sys, bd02)
 add!(sys, bd03)
 
+add!(sys, jnt01)
+add!(sys, jnt02)
+add!(sys, jnt03)
+
 add!(sys, jnt11)
 add!(sys, jnt2)
 add!(sys, jnt21)
@@ -85,13 +95,13 @@ add!(sys, jnt31)
 add!(sys, jnt4)
 add!(sys, jnt5)
 
-# add!(sys, tcp11)
-# add!(sys, tcp2)
-# add!(sys, tcp21)
-# add!(sys, tcp3)
-# add!(sys, tcp31)
-# add!(sys, tcp4)
-# add!(sys, tcp5)
+add!(sys, tcp11)
+add!(sys, tcp2)
+add!(sys, tcp21)
+add!(sys, tcp3)
+add!(sys, tcp31)
+add!(sys, tcp4)
+add!(sys, tcp5)
 
 if (!assemble!(sys))
     println("Assembling failed!")
@@ -148,8 +158,14 @@ mass = zeros(number_of_dofs(sys), number_of_dofs(sys));
 for i in 1:last_body_dof(sys)
     mass[i, i] = 1
 end
-time_span = 0:0.005:20
+time_span = 0:0.005:10
 sol1 = Matrix{Float64}(undef, number_of_dofs(sys), length(time_span))
 cros!(sol1, initial, mass, func, jacoby, step(time_span))
 
-animate(sys, sol1, time_span, "real_5bar_stat.mp4"; framerate = 60, limits = (-7,7, -7, 7))
+animate(sys, sol1, time_span, "real_5bar_dyn.mp4"; framerate = 60, limits = (-7,7, -7, 7))
+
+sol2 = Matrix{Float64}(undef, number_of_dofs(sys), length(time_span))
+
+static_solver!( sol2 , initial, func, jacoby)
+
+animate(sys, sol2, time_span, "real_5bar_stat.mp4"; framerate = 60, limits = (-7,7, -7, 7))
